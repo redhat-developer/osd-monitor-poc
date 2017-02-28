2cd9f4495fe741e3475493c0f6fb66d0b8f5e822
Karanbir Singh <kbsingh@karan.org>
2017-02-28 16:16:18 +0000

    we need docker installed and running as well

--- cico_deploy.sh
+++ cico_deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+yum -y install docker 
+service docker start
+
 cd grafana-hawkular
-Docker build -t registry.devshift.net/perf/osd-monitor:latest . 
+docker build -t registry.devshift.net/perf/osd-monitor:latest . 
 if [ $? -eq 0 ]; then
   docker push registry.devshift.net/perf/osd-monitor:latest
   rtn=$?
