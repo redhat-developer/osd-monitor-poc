865b1037f296d3469d85528a3277ef0fa263111c
Frank Ch. Eigler <fche@redhat.com>
2017-03-22 13:22:08 -0400

    add pmmgr-based pcp-central-logger container

--- cico_build.sh
+++ cico_build.sh
@@ -10,9 +10,14 @@ service docker start
     docker build -t registry.devshift.net/perf/osd-monitor:latest . 
     docker push registry.devshift.net/perf/osd-monitor:latest
 )
-
 (
     cd pcp-node-collector
     docker build -t registry.devshift.net/perf/pcp-node-collector:latest . 
     docker push registry.devshift.net/perf/pcp-node-collector:latest
 )
+(
+    cd pcp-central-logger
+    docker build -t registry.devshift.net/perf/pcp-central-logger:latest . 
+    docker push registry.devshift.net/perf/pcp-central-logger:latest
+)
+
