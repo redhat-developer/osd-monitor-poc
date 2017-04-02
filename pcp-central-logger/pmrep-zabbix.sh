4dbb04d7fe1d1951e0f19ff7a8c3798ee413b191
Frank Ch. Eigler <fche@redhat.com>
2017-04-02 13:32:55 -0400

    add separated node-vs-pod pmmgr configurations, zabbix feed for former

---
+++ pcp-central-logger/pmrep-zabbix.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+# This is a wrapper for pmrep.  We want to send out zabbix data for a
+# given incoming $PCP_HOST, while propagating the pmcd.hostname of
+# that server to zabbix.  And we want to run multiple copies of this
+# in parallel.
+#
+# We assume a $PCP_HOST env var is set as a hostspec (pcp -h $PCP_HOST).
+# We assume the $ZABBIX_SERVER env var is set.
+
+tmpfile=`mktemp`.conf
+trap 'rm -f $tmpfile; exit' 0 1 2 3 5 9 15
+
+if [ -z "$ZABBIX_SERVER" ]; then echo need ZABBIX_SERVER; exit 1; fi
+if [ -z "$PCP_HOST" ]; then echo need PCP_HOST; exit 1; fi
+
+pcp_hostname=`pminfo -f pmcd.hostname | grep value | cut -f2 -d'"'` 
+
+(
+echo '[options]'
+echo zabbix_server = $ZABBIX_SERVER
+echo zabbix_host = $pcp_hostname
+echo zabbix_interval = 60s
+) > $tmpfile
+
+echo starting pmrep for $PCP_HOST to $ZABBIX_HOST
+pmrep -c $tmpfile -o zabbix $@
