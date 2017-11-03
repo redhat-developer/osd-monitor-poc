f68044c361bf6dbee2ee208f43a9066a6f4aa4b9
Frank Ch. Eigler <fche@redhat.com>
2017-11-03 18:11:36 -0400

    snappy

--- oso-central-logger/pmmgr+pmcd.sh
+++ oso-central-logger/pmmgr+pmcd.sh
@@ -15,6 +15,7 @@ echo "proc     3  dso  proc_init   $PCP_PMDAS_DIR/proc/pmda_proc.so"   >> $PCP_P
 echo "linux   60  dso  linux_init  $PCP_PMDAS_DIR/linux/pmda_linux.so" >> $PCP_PMCDCONF_PATH;
 echo "prometheus 144 pipe binary python $PCP_PMDAS_DIR/prometheus/pmdaprometheus.python" >> $PCP_PMCDCONF_PATH;
 rm -f $PCP_VAR_DIR/pmns/root_xfs $PCP_VAR_DIR/pmns/root_jbd2 $PCP_VAR_DIR/pmns/root_root $PCP_VAR_DIR/pmns/root
+echo 'prometheus	144:*:*' > $PCP_VAR_DIR/pmns/prometheus
 touch $PCP_VAR_DIR/pmns/.NeedRebuild
 
 # allow unauthenticated access to proc.* metrics (default is false)
@@ -22,8 +23,10 @@ export PROC_ACCESS=1
 export PMCD_ROOT_AGENT=0
 
 # NB: we can't use the rc.pmcd script.  It assumes that it's run as root.
+# NB: we can't run the pmda Install scripts.  They assume ping etc. are available.
 cd $PCP_VAR_DIR/pmns
 ./Rebuild
+pmnsadd -n root prometheus
 
 cd $PCP_LOG_DIR
 
@@ -34,6 +37,12 @@ exec /usr/libexec/pcp/bin/pmcd -l /dev/force-logging-to-stderr -f -A -H $PCP_HOS
 sleep 5 # give time for pmcd's startup messages, so it doesn't intermix with pmmgr's
 
 # XXX: loop regenerating the tenant list, regenerating pmmgr
-
+(. /etc/pcp.env;
+ while true
+ do
+     /regen-tenant-list.sh
+     sleep 60
+ done) &
+    
 exec $VALGRIND /usr/libexec/pcp/bin/pmmgr -v -p 180 -c /etc/pcp/pmmgr-tenant
 # XXX: one pmmgr configuration directory per tenant-class
