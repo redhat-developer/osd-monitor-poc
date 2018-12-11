cb8e3c6dae0527a486c5bf441341d37e45e3b194
Frank Ch. Eigler <fche@redhat.com>
2018-12-11 11:18:14 -0500

    adapt to post pcp 4.2 pmdapostgresql

--- pcp-postgresql-monitor/pmcd.sh
+++ pcp-postgresql-monitor/pmcd.sh
@@ -13,9 +13,7 @@ echo "# Name  ID  IPC  IPC Params  File/Cmd" >> $PCP_PMCDCONF_PATH;
 echo "pmcd     2  dso  pmcd_init   $PCP_PMDAS_DIR/pmcd/pmda_pmcd.so"   >> $PCP_PMCDCONF_PATH;
 echo "proc     3  dso  proc_init   $PCP_PMDAS_DIR/proc/pmda_proc.so"   >> $PCP_PMCDCONF_PATH;
 echo "linux   60  dso  linux_init  $PCP_PMDAS_DIR/linux/pmda_linux.so" >> $PCP_PMCDCONF_PATH;
-echo "postgresql      110     pipe    binary          perl /var/lib/pcp/pmdas/postgresql/pmdapostgresql.pl" >> $PCP_PMCDCONF_PATH;
-#                                                                                                         ^^^^^
-#                                                                           for stderr logging, need also: -l -, but no perl/pmda support yet
+echo "postgresql      110     pipe    binary       python /var/lib/pcp/pmdas/postgresql/pmdapostgresql.python -l -" >> $PCP_PMCDCONF_PATH;
 
 rm -f $PCP_VAR_DIR/pmns/root_xfs $PCP_VAR_DIR/pmns/root_jbd2 $PCP_VAR_DIR/pmns/root_root $PCP_VAR_DIR/pmns/root
 echo 'postgresql	110:*:*' > $PCP_VAR_DIR/pmns/postgresql
@@ -31,11 +29,22 @@ cd $PCP_VAR_DIR/pmns
 $PCP_BINADM_DIR/pmnsadd -n root postgresql
 
 cd $PCP_PMDAS_DIR/postgresql
+
+# pcp < 4.2ish
 conf=postgresql.conf
 echo '$database="dbi:Pg:dbname='$DB_DB';host='$DB_HOSTNAME';port='$DB_PORT'";' > $conf
 echo '$username="'$DB_USER'";' >> $conf
 echo '$password='"'"$DB_PASSWORD"'"';' >> $conf # protect $punctuation within
 echo '$os_user="'`whoami`'";' >> $conf
+
+# pcp >= 4.2ish
+conf=pmdapostgresql.conf
+echo '[authentication]' > $conf
+echo "host=$DB_HOSTNAME" >> $conf
+echo "port=$DB_PORT" >> $conf
+echo "dbname=$DB_DB" >> $conf
+echo "user=$DB_USER" >> $conf
+echo "password=$DB_PASSWORD" >> $conf
 
 cd $PCP_LOG_DIR
 exec $PCP_BINADM_DIR/pmcd -p $PMCD_PORT -l - -f -A -H $PCP_HOSTNAME
