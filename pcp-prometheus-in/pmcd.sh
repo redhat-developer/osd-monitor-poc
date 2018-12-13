9420a90e6f79e3184dfbae8123cc0ba2fd98d01e
Frank Ch. Eigler <fche@redhat.com>
2018-12-12 22:08:08 -0500

    pcp-prometheus-in: correct missing 'notready' pmcd.conf flag for pmdaprometheus

--- pcp-prometheus-in/pmcd.sh
+++ pcp-prometheus-in/pmcd.sh
@@ -16,7 +16,7 @@ echo "# Name  ID  IPC  IPC Params  File/Cmd" >> $PCP_PMCDCONF_PATH;
 echo "pmcd     2  dso  pmcd_init   $PCP_PMDAS_DIR/pmcd/pmda_pmcd.so"   >> $PCP_PMCDCONF_PATH;
 echo "proc     3  dso  proc_init   $PCP_PMDAS_DIR/proc/pmda_proc.so"   >> $PCP_PMCDCONF_PATH;
 echo "linux   60  dso  linux_init  $PCP_PMDAS_DIR/linux/pmda_linux.so" >> $PCP_PMCDCONF_PATH;
-echo "prometheus 144 pipe binary env PCP_DEBUG=all python $PCP_PMDAS_DIR/prometheus/pmdaprometheus.python -D -l -" >> $PCP_PMCDCONF_PATH;
+echo "prometheus 144 pipe binary notready python $PCP_PMDAS_DIR/prometheus/pmdaprometheus.python -l - -D " >> $PCP_PMCDCONF_PATH;
 rm -f $PCP_VAR_DIR/pmns/root_xfs $PCP_VAR_DIR/pmns/root_jbd2 $PCP_VAR_DIR/pmns/root_root $PCP_VAR_DIR/pmns/root
 echo 'prometheus	144:*:*' > $PCP_VAR_DIR/pmns/prometheus
 touch $PCP_VAR_DIR/pmns/.NeedRebuild
@@ -38,4 +38,4 @@ cd $PCP_LOG_DIR
 
 : "${PCP_HOSTNAME:=`uname -n`}"
 : "${PMCD_TIMEOUT:=300}"
-exec /usr/libexec/pcp/bin/pmcd -l - -f -t $PMCD_TIMEOUT -p $PMCD_PORT -A -Dall -H $PCP_HOSTNAME
+exec /usr/libexec/pcp/bin/pmcd -l - -f -t $PMCD_TIMEOUT -p $PMCD_PORT -A -H $PCP_HOSTNAME
